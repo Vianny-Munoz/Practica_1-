c3eaef2b41dbdb4ab3625b4c363181587a35030a
Jdyg80 <125572709+Jdyg80@users.noreply.github.com>
2025-03-07 23:52:33 -0500

    modulo administrativo.py  (#6)
    * se agrego la clase administrativo
    
    * Agregando modulo administrativo.py
    
    * administrativo.py
    
    * agregando solo administrativo.py
    
    * Eliminando submódulo Practica_1-
    
    * Solo administrativo.py
    
    * Agregando correctamente administrativo.py
    
    ---------
    
    Co-authored-by: Tania <madeeelin@gmail.com>
    Co-authored-by: Jdyg80 <jordyjfk47@gmail.comq>

---
+++ administrativo.py
@@ -0,0 +1,40 @@
+from Persona import Persona
+
+class Administrativo(Persona):
+    def __init__(self, nombre, edad, direccion, area, cargo):
+        super().__init__(nombre, edad, direccion)
+        self.__area = area
+        self.__cargo = cargo
+    
+    def getarea(self):
+        return self.__area
+        
+    def setarea(self, area):
+        self.__area=area
+
+    def getcargo(self):
+        return self.__cargo
+        
+    def setcargo(self, cargo):
+        self.__cargo=cargo
+
+    def __str__(self):
+    
+        print(f"Los datos del Administrativo son: \n", super().__str__(), "\n Ubicado en el area: ", self.getarea, "\n Con cargo: ", self.getcargo)
+
+class GestorAdministrativos:
+    def __init__(self):
+        self.administrativos = {}
+
+    def agregar(self, nombre, edad, direccion, area, cargo):
+        if nombre in self.administrativos:
+            return False
+        self.administrativos[nombre] = Administrativo(nombre, nombre, edad, direccion, area, cargo)
+        return True
+
+    def eliminar(self, nombre):
+        if nombre not in self.administrativos:
+            return False
+        del self.administrativos[nombre]
+        return True
+ 
